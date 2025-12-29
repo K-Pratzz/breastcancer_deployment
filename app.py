182b2f033a3892a7491b050b724597acb10090f3
Kumari Pratibha <kp939331@gmail.com>
2025-12-29 19:19:19 +0530

    fixed input data

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
+
+
 model=joblib.load("BreastCancer_Model.pkl")
 st.title("Breast Cancer Detection")
 st.header("Enter the details")
@@ -66,18 +69,44 @@ symmetry_worst = st.number_input("Enter symmetry_worst", min_value=0.1565, max_v
 fractal_dimension_worst = st.number_input("Enter fractal_dimension_worst", min_value=0.05504, max_value=0.2075,value=0.05504)
 
 if st.button("Predict"):
-    data=np.array([[radius_mean,texture_mean,perimeter_mean,
-       area_mean,smoothness_mean,compactness_mean,concavity_mean,
-       concave_points_mean,symmetry_mean,fractal_dimension_mean,
-       radius_se,texture_se,perimeter_se,area_se,smoothness_se,
-       compactness_se,concavity_se,concave_points_se,symmetry_se,
-       fractal_dimension_se,radius_worst,texture_worst,
-       perimeter_worst, area_worst,smoothness_worst,
-       compactness_worst,concavity_worst,concave_points_worst,
-       symmetry_worst,fractal_dimension_worst]],dtype=float)
+    input_data = {
+    "radius_mean": radius_mean,
+    "texture_mean": texture_mean,
+    "perimeter_mean": perimeter_mean,
+    "area_mean": area_mean,
+    "smoothness_mean": smoothness_mean,
+    "compactness_mean": compactness_mean,
+    "concavity_mean": concavity_mean,
+    "concave_points_mean": concave_points_mean,
+    "symmetry_mean": symmetry_mean,
+    "fractal_dimension_mean": fractal_dimension_mean,
+    "radius_se": radius_se,
+    "texture_se": texture_se,
+    "perimeter_se": perimeter_se,
+    "area_se": area_se,
+    "smoothness_se": smoothness_se,
+    "compactness_se": compactness_se,
+    "concavity_se": concavity_se,
+    "concave_points_se": concave_points_se,
+    "symmetry_se": symmetry_se,
+    "fractal_dimension_se": fractal_dimension_se,
+    "radius_worst": radius_worst,
+    "texture_worst": texture_worst,
+    "perimeter_worst": perimeter_worst,
+    "area_worst": area_worst,
+    "smoothness_worst": smoothness_worst,
+    "compactness_worst": compactness_worst,
+    "concavity_worst": concavity_worst,
+    "concave_points_worst": concave_points_worst,
+    "symmetry_worst": symmetry_worst,
+    "fractal_dimension_worst": fractal_dimension_worst
+    }
+
+    data = pd.DataFrame([input_data])
     
     ans=model.predict(data)
     if ans[0]==1:
         st.error("The answer is Malignant(Cancerous)")
     else:
         st.success("The answer is Benign(Non-Cancerous)")
+
